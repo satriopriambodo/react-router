e8c733ec719f76feb4526ab8c4e2c94f73fe567c
satrio priambodo <satriopriambodo96@gmail.com>
2022-09-14 13:30:33 +0700

    -

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Product";
+import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/product-detail/:productId">
+          <ProductDetail />
+        </Route>
+      </main>
     </div>
   );
 }
---
+++ src/pages/Product.js
@@ -0,0 +1,14 @@
+const Products = () => {
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <li>A Book</li>
+        <li>A Bag</li>
+        <li>A Wallet</li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
